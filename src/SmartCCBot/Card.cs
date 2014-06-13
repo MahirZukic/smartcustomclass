7bd587f2f3836841cf5cb359382b74bfedf575b2
Wirmate <Wirmate@gmail.com>
2014-06-13 07:01:02 +0000

    Simulation fix

--- src/SmartCCBot/Card.cs
+++ src/SmartCCBot/Card.cs
@@ -48,8 +48,8 @@ namespace HREngine.Bots
             else if (Type == CType.WEAPON)
             {
                 value += (ValuesInterface.ValueAttackWeapon * CurrentAtk) /** (ValuesInterface.ValueDurabilityWeapon * CurrentDurability)*/;
-                if (CurrentAtk > 1)
-                    value += ValuesInterface.ValueDurabilityWeapon * CurrentDurability;
+                value += ValuesInterface.ValueDurabilityWeapon * CurrentDurability;
+
             }
 
             return value;
