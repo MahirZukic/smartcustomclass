efcd091efeb00bb57d7fe0f3e76377b5e2dfba05
Wirmate@gmail.com <Wirmate@gmail.com@6845b56e-594f-51a2-22d8-0edb722f2fdc>
2014-06-19 21:47:03 +0000

    Fix Tundra Rhino

--- trunk/src/SmartCCBot/Cards/DS1_178.cs
+++ trunk/src/SmartCCBot/Cards/DS1_178.cs
@@ -8,18 +8,20 @@ using System.Text;
 namespace HREngine.Bots
 {
     [Serializable]
-public class DS1_178 : Card
+    public class DS1_178 : Card
     {
-		public override Card Create()
-{ return new DS1_178();}
-public DS1_178() : base()
+        public override Card Create()
+        { return new DS1_178(); }
+        public DS1_178()
+            : base()
         {
-            
+
         }
-		
-        public DS1_178(CardTemplate newTemplate, bool isFriend, int id) : base(newTemplate,isFriend,id)
+
+        public DS1_178(CardTemplate newTemplate, bool isFriend, int id)
+            : base(newTemplate, isFriend, id)
         {
-            
+
         }
 
         public override void Init()
@@ -28,28 +30,33 @@ public DS1_178() : base()
             IsCharge = true;
         }
 
-        public override void OnPlay(ref Board board, Card target = null,int index = 0,int choice = 0)
+        public override void OnPlay(ref Board board, Card target = null, int index = 0, int choice = 0)
         {
-            base.OnPlay(ref board, target,index);
+            base.OnPlay(ref board, target, index);
         }
 
         public override void OnDeath(ref Board board)
         {
             base.OnDeath(ref board);
+            foreach (Card c in board.MinionFriend)
+            {
+                if (c.Race == CRace.BEAST)
+                    c.IsCharge = false;
+            }
         }
 
         public override void OnPlayOtherMinion(ref Board board, ref Card Minion)
         {
-            base.OnPlayOtherMinion(ref board,ref Minion);
+            base.OnPlayOtherMinion(ref board, ref Minion);
             if (Minion.Race == CRace.BEAST && Minion.IsFriend == IsFriend)
                 Minion.IsCharge = true;
         }
 
         public override void OnCastSpell(ref Board board, Card Spell)
         {
-		    base.OnCastSpell(ref board, Spell);
+            base.OnCastSpell(ref board, Spell);
         }
 
-		
+
     }
 }
--- trunk/src/SmartCCBot/Cards/EX1_556.cs
+++ trunk/src/SmartCCBot/Cards/EX1_556.cs
@@ -8,18 +8,20 @@ using System.Text;
 namespace HREngine.Bots
 {
     [Serializable]
-public class EX1_556 : Card
+    public class EX1_556 : Card
     {
-		public override Card Create()
-{ return new EX1_556();}
-public EX1_556() : base()
+        public override Card Create()
+        { return new EX1_556(); }
+        public EX1_556()
+            : base()
         {
-            
+
         }
-		
-        public EX1_556(CardTemplate newTemplate, bool isFriend, int id) : base(newTemplate,isFriend,id)
+
+        public EX1_556(CardTemplate newTemplate, bool isFriend, int id)
+            : base(newTemplate, isFriend, id)
         {
-            
+
         }
 
         public override void Init()
@@ -32,9 +34,9 @@ public EX1_556() : base()
             return base.GetValue(board) + 5;
         }
 
-        public override void OnPlay(ref Board board, Card target = null,int index = 0,int choice = 0)
+        public override void OnPlay(ref Board board, Card target = null, int index = 0, int choice = 0)
         {
-            base.OnPlay(ref board, target,index);
+            base.OnPlay(ref board, target, index);
         }
 
         public override void OnDeath(ref Board board)
@@ -45,13 +47,13 @@ public EX1_556() : base()
 
         public override void OnPlayOtherMinion(ref Board board, ref Card Minion)
         {
-            base.OnPlayOtherMinion(ref board,ref Minion);
+            base.OnPlayOtherMinion(ref board, ref Minion);
         }
 
         public override void OnCastSpell(ref Board board, Card Spell)
         {
-		    base.OnCastSpell(ref board, Spell);
+            base.OnCastSpell(ref board, Spell);
         }
-				
+
     }
 }
