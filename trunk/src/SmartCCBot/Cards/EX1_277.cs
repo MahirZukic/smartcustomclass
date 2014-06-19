11f515f66ec08f1d130eaf0497a79fb1142a07ff
Wirmate@gmail.com <Wirmate@gmail.com@6845b56e-594f-51a2-22d8-0edb722f2fdc>
2014-06-19 23:18:09 +0000

    tweak Arcane Missiles

--- trunk/src/SmartCCBot/Cards/EX1_277.cs
+++ trunk/src/SmartCCBot/Cards/EX1_277.cs
@@ -8,18 +8,20 @@ using System.Text;
 namespace HREngine.Bots
 {
     [Serializable]
-public class EX1_277 : Card
+    public class EX1_277 : Card
     {
-		public override Card Create()
-{ return new EX1_277();}
-public EX1_277() : base()
+        public override Card Create()
+        { return new EX1_277(); }
+        public EX1_277()
+            : base()
         {
-            
+
         }
-		
-        public EX1_277(CardTemplate newTemplate, bool isFriend, int id) : base(newTemplate,isFriend,id)
+
+        public EX1_277(CardTemplate newTemplate, bool isFriend, int id)
+            : base(newTemplate, isFriend, id)
         {
-            
+
         }
 
         public override void Init()
@@ -27,9 +29,9 @@ public EX1_277() : base()
             base.Init();
         }
 
-        public override void OnPlay(ref Board board, Card target = null,int index = 0,int choice = 0)
+        public override void OnPlay(ref Board board, Card target = null, int index = 0, int choice = 0)
         {
-            base.OnPlay(ref board, target,index);
+            base.OnPlay(ref board, target, index);
             board.DoRandomDamage(1, false);
             board.DoRandomDamage(1, false);
             board.DoRandomDamage(1, false);
@@ -44,13 +46,13 @@ public EX1_277() : base()
 
         public override void OnPlayOtherMinion(ref Board board, ref Card Minion)
         {
-            base.OnPlayOtherMinion(ref board,ref Minion);
+            base.OnPlayOtherMinion(ref board, ref Minion);
         }
 
         public override void OnCastSpell(ref Board board, Card Spell)
         {
-		    base.OnCastSpell(ref board, Spell);
+            base.OnCastSpell(ref board, Spell);
         }
-		
+
     }
 }
