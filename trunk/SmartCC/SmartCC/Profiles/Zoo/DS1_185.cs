749ced28fd1be083b735317e9c5241a33c1681d9
Wirmate@gmail.com <Wirmate@gmail.com@6845b56e-594f-51a2-22d8-0edb722f2fdc>
2014-06-28 23:37:50 +0000

    New Rush profile, new zoo profile, fix for wild growth when used at 10 mana, minor fixes

---
+++ trunk/SmartCC/SmartCC/Profiles/Zoo/DS1_185.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+//Arcane Shot
+
+namespace HREngine.Bots
+{
+	[Serializable]
+    public class bDS1_185 : Behavior
+    {
+		public bDS1_185() : base()
+        {
+            
+        }
+		
+		public override bool ShouldBePlayed(Board board)
+        {
+            return true;
+        }
+
+        public override bool ShouldAttack(Board board)
+        {
+            return true;
+        }
+
+        public override bool ShouldAttackTarget(Card target)
+        {
+            return true;
+        }
+		
+		public override bool ShouldBePlayedOnTarget(Card target)
+        {
+            return true;
+        }
+
+        public override int GetPriorityPlay(Board board)
+        {
+            return 1;
+        }
+		
+    }
+}
---
+++ trunk/SmartCC/SmartCC/Profiles/Zoo/EX1_093.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+//Defender of Argus
+
+namespace HREngine.Bots
+{
+	[Serializable]
+    public class bEX1_093 : Behavior
+    {
+		public bEX1_093() : base()
+        {
+            
+        }
+		
+		public override bool ShouldBePlayed(Board board)
+        {
+			if(board.MinionFriend.Count < 1)
+			{
+				if(board.Hand.Count >= 3)
+					return false;
+			}
+            return true;
+        }
+
+        public override bool ShouldAttack(Board board)
+        {
+            return true;
+        }
+
+        public override bool ShouldAttackTarget(Card target)
+        {
+            return true;
+        }
+		
+		public override bool ShouldBePlayedOnTarget(Card target)
+        {
+            return true;
+        }
+
+        public override int GetPriorityPlay(Board board)
+        {
+            return 1;
+        }
+		
+    }
+}
