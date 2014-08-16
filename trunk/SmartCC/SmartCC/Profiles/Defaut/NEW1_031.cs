fa9f212fd825a7ae49aee8c0e6dc2f2e236dbd11
Wirmate@gmail.com <Wirmate@gmail.com@6845b56e-594f-51a2-22d8-0edb722f2fdc>
2014-08-16 10:19:21 +0000

    Performances improved New logic added to not waste worthy minions (dont play knife juggler and play weaker minion instead if enemy has a  2/1 on board for exemple) -> may sound obvious but bot wasnt aware of this before

--- trunk/SmartCC/SmartCC/Profiles/Defaut/NEW1_031.cs
+++ trunk/SmartCC/SmartCC/Profiles/Defaut/NEW1_031.cs
@@ -18,6 +18,8 @@ namespace HREngine.Bots
 		
 		public override bool ShouldBePlayed(Board board)
         {
+			if(board.MinionFriend.Count == 7)
+				return false;
             return true;
         }
 
